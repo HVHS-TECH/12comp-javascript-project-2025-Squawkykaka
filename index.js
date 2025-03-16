9e3cde90def1fa28a8f660fd6022060c4318ff90
Squawkykaka <squawkykaka@gmail.com>
2025-03-17 12:21:54 +1300

    code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ gameState = 0
 gameStateSetup = false
 
 function setup() {
-
+    const CANVAS = new Canvas(windowWidth, windowHeight)
 }
 
 function draw() {
@@ -19,12 +19,34 @@ function gameManager() {
         case 0:
             mainMenu()
             break;
+        case 1:
+            instructionsScreen()
+            break;
     }
+}
+
+function changeGameState(newGameState) {
+    allSprites.remove()
+    gameStateSetup = false
+    gameState = newGameState
 }
 
 function mainMenu() {
     if(gameStateSetup === false) {
-        spawnButton("Start Game", windowWidth/2, windowHeight/2)
+        spawnButton("Instructions", windowWidth/2-250, windowHeight/2, ()=>{
+            console.log("hi")
+            changeGameState(1)
+        })
+        gameStateSetup = true
+    }
+}
+
+function instructionsScreen() {
+    if(gameStateSetup === false) {
+        spawnButton("Go Home", 100, 50, ()=>{
+            console.log("hi")
+            changeGameState(0)
+        })
         gameStateSetup = true
     }
 }
