b81afd3dae95f052a4d937691a6d7fbacd2e24a5
Squawkykaka <squawkykaka@gmail.com>
2025-03-16 16:31:21 +1300

    make swing work

--- scripts/asteroids.js
+++ scripts/asteroids.js
@@ -1,12 +1,20 @@
+let lanceCreated = false
+let joint;
+
 function createAsteroid(x, y) {
-    asteroid = new Sprite(x,y, random(30, 70))
+    asteroid = new Sprite(x,y, random(100, 300))
     asteroid.color = "red"
 
     asteroid.update = () => {
-        if (asteroid.mouse.pressing()) {
+        if (asteroid.mouse.pressing() && lanceCreated == false) {
             joint = new DistanceJoint(player, asteroid)
+            lanceCreated = true
         }
     }
-
     asteroidGroup.add(asteroid)
+}
+
+function mouseReleased() {
+    joint.remove()
+    lanceCreated = false
 }
