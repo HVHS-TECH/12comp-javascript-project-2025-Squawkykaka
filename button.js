0992641b073ffafa356c480574d375de5eb1deec
Squawkykaka <squawkykaka@gmail.com>
2025-03-17 12:21:54 +1300

    code

--- button.js
+++ button.js
@@ -1,4 +1,8 @@
-function spawnButton(text, x, y) {
+function spawnButton(text, x, y, func) {
     button = new Sprite(x, y, textWidth(text)+100, 50)
     button.text = text
+    button.update = () => {
+        if(button.mouse.pressing()) {func(); button.color = "blue"}
+        else button.color = "red"
+    }
 }
