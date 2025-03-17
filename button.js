aa0ebe32165eb38065ab6d4df207f06701af8e39
Squawkykaka <squawkykaka@gmail.com>
2025-03-17 18:05:54 +1300

    made berrys fall

--- button.js
+++ button.js
@@ -1,8 +1,11 @@
 function spawnButton(text, x, y, func) {
-    button = new Sprite(x, y, textWidth(text)+100, 50)
+    button = new Sprite(x, y, textWidth(text)+100, 50, "k")
     button.text = text
+
+    buttonGroup.add(button)
+    
     button.update = () => {
-        if(button.mouse.pressing()) {func(); button.color = "blue"}
+        if(button.mouse.pressing()) {func();}
         else button.color = "red"
     }
 }
