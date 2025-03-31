713cef18f58c50c2d9700cad95c65450ad52c980
Squawkykaka <squawkykaka@gmail.com>
2025-04-01 12:53:46 +1300

    Format

--- berrys.js
+++ berrys.js
@@ -1,8 +1,13 @@
-function spawnBerry(x, y) {
-    berry = new Sprite(x, y, 20, 20)
-    berry.life = 60*5
-    berry.image = berryImage
-    berry.image.scale = 0.2
-    
-    berryGroup.add(berry)
+function spawnBerry(x, y, xVel) {
+  berry = new Sprite(x, y, 20, 20);
+  berry.life = 60 * 5;
+  berry.vel.x = xVel;
+  berry.image = berryImage;
+  berry.image.scale = 0.2;
+
+  berry.update = () => {
+    berry.vel.x = xVel;
+  };
+
+  berryGroup.add(berry);
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-gameState = 0
+gameState = 0;
 
 let buttonGroup;
 let berryGroup;
@@ -10,50 +10,50 @@ let bear, floor;
 let timeHoldingSpace = 0;
 
 function preload() {
-    berryImage = loadImage('./assets/berry.jpg')
-    bearImage = loadImage('./assets/bear.jpg')
+  berryImage = loadImage("./assets/berry.jpg");
+  bearImage = loadImage("./assets/bear.jpg");
 }
 
 function setup() {
-    const CANVAS = new Canvas(windowWidth, windowHeight)
-    world.gravity.y = 10;
+  const CANVAS = new Canvas(windowWidth, windowHeight);
+  world.gravity.y = 10;
 
-    buttonGroup = new Group()
-    berryGroup = new Group()
+  buttonGroup = new Group();
+  berryGroup = new Group();
 
-    buttonGroup.overlaps(allSprites)
+  buttonGroup.overlaps(allSprites);
 
-    setupGameScreen()
+  setupGameScreen();
 }
 
 function draw() {
-    gameManager()
+  gameManager();
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function gameManager() {
-    switch(gameState) {
-        case 0:
-            mainMenu()
-            break;
-        case 1:
-            instructionsScreen()
-            break;
-        case 2:
-            gameScreen()
-            break;
-    }
+  switch (gameState) {
+    case 0:
+      mainMenu();
+      break;
+    case 1:
+      instructionsScreen();
+      break;
+    case 2:
+      gameScreen();
+      break;
+  }
 }
 
 function changeGameState(setupFunction) {
-    world.gravity.y = 10;
-    textAlign(LEFT)
-    textSize(20)
-    allSprites.remove()
-    setupFunction()
+  world.gravity.y = 10;
+  textAlign(LEFT);
+  textSize(20);
+  allSprites.remove();
+  setupFunction();
 }
 
 function mainMenu() {
@@ -62,30 +62,37 @@ function mainMenu() {
 }
 
 function instructionsScreen() {
-    background(120)
-    spawnBerry(random(windowWidth), -20)
+  background(120);
+  spawnBerry(random(windowWidth), -20, 0);
 
-    berryGroup.draw()
-    text("Use WASD keys to move and F for ability, \nif you get hit by a bear you get hurt, so avoid bears\nCollect berries for score and find cool upgrades", windowWidth/2, windowHeight/2)
-    // stuff + thingd
+  berryGroup.draw();
+  text(
+    "Use WASD keys to move and F for ability, \nif you get hit by a bear you get hurt, so avoid bears\nCollect berries for score and find cool upgrades",
+    windowWidth / 2,
+    windowHeight / 2
+  );
+  // stuff + thingd
 }
 
 function gameScreen() {
     background(119)
 
-    // if(random(1, 500) <= 3) {
-    //     createBear()
-    //     bearAttack = true;
-    // }
+  if (random(1, 500) <= 3) {
+    createBear();
+    bearAttack = true;
+  }
+  if (random(1, 500) <= 3) {
+    spawnBerry(windowWidth, random(windowHeight), -10);
+  }
 
-    if(bearAttack) {
-        bear.visible = true;
-        bear.vel.x = -10;
-        if (bear.x <= -200) {
-            bear.remove()
-        }
+  if (bearAttack) {
+    bear.visible = true;
+    bear.vel.x = -10;
+    if (bear.x <= -200) {
+      bear.remove();
     }
-    playerMovement()
+  }
+  playerMovement();
 }
 
 function playerMovement() {
@@ -103,45 +110,60 @@ function playerMovement() {
 }
 
 function setupMainMenu() {
-    gameState=0
-    console.log("pre instructions")
-    spawnButton("Instructions", windowWidth/2-250, windowHeight/2, (button) => {
-        console.log("instuctions")
-        changeGameState(setupInstructionsScreen)
-    })
-    console.log("pre play game")
-    spawnButton("Play Game", windowWidth/2+250, windowHeight/2, (button) => {
-        console.log("playgame")
-        changeGameState(setupGameScreen)
-    })
+  gameState = 0;
+  console.log("pre instructions");
+  spawnButton(
+    "Instructions",
+    windowWidth / 2 - 250,
+    windowHeight / 2,
+    (button) => {
+      console.log("instuctions");
+      changeGameState(setupInstructionsScreen);
+    }
+  );
+  console.log("pre play game");
+  spawnButton(
+    "Play Game",
+    windowWidth / 2 + 250,
+    windowHeight / 2,
+    (button) => {
+      console.log("playgame");
+      changeGameState(setupGameScreen);
+    }
+  );
 }
 
 function setupInstructionsScreen() {
-    gameState = 1
-    textAlign(CENTER)
+  gameState = 1;
+  textAlign(CENTER);
 
-    spawnButton("Go Home", 100, 50, () => {
-        console.log("hi")
-        changeGameState(setupMainMenu)
-    })
+  spawnButton("Go Home", 100, 50, () => {
+    console.log("hi");
+    changeGameState(setupMainMenu);
+  });
 }
 
 function setupGameScreen() {
-    gameState = 2
+  gameState = 2;
 
-    spawnButton("Go Home", 100, 50, () => {
-        console.log("hi")
-        changeGameState(setupMainMenu)
-    })
-    floor = new Sprite(windowWidth/2, windowHeight-20, windowWidth*2, 1, "K")
+  spawnButton("Go Home", 100, 50, () => {
+    console.log("hi");
+    changeGameState(setupMainMenu);
+  });
+  floor = new Sprite(
+    windowWidth / 2,
+    windowHeight - 20,
+    windowWidth * 2,
+    1,
+    "K"
+  );
 
-    player = new Sprite(windowWidth/2, windowHeight-50, 50, 50)
+  player = new Sprite(windowWidth / 2, windowHeight - 50, 50, 50);
 }
 
 function createBear() {
-    bear = new Sprite(windowWidth-200, windowHeight-200, 120)
-    bear.image = bearImage
-    bear.image.scale = 0.4
-    bear.visible = false;
-    return bear;
+  bear = new Sprite(windowWidth - 200, windowHeight - 200, 120);
+  bear.image = bearImage;
+  bear.image.scale = 0.4;
+  bear.visible = false;
 }
