83dd45f70fb0bc29d82f19b173df5b8f71097f0e
Squawkykaka <squawkykaka@gmail.com>
2025-04-01 12:53:46 +1300

    Format

--- button.js
+++ button.js
@@ -1,13 +1,13 @@
 function spawnButton(text, x, y, func) {
-    let button = new Sprite(x, y, textWidth(text) + 100, 50, "k");
-    button.text = text;
-    button.textSize = 20;
+  let button = new Sprite(x, y, textWidth(text) + 100, 50, "k");
+  button.text = text;
+  button.textSize = 20;
 
-    button.onClickFunction = func; // Store function separately
+  button.onClickFunction = func; // Store function separately
 
-    button.update = () => {
-        if (button.mouse.pressing()) button.onClickFunction(button);
-    };
+  button.update = () => {
+    if (button.mouse.pressing()) button.onClickFunction(button);
+  };
 
-    buttonGroup.add(button);
+  buttonGroup.add(button);
 }
