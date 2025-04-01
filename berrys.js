522433df0199ae6b2f51116e4af64b9f5614c330
Squawkykaka <squawkykaka@gmail.com>
2025-04-01 13:03:00 +1300

    Stuff

--- berrys.js
+++ berrys.js
@@ -1,13 +1,17 @@
 function spawnBerry(x, y, xVel) {
-  berry = new Sprite(x, y, 20, 20);
-  berry.life = 60 * 5;
-  berry.vel.x = xVel;
-  berry.image = berryImage;
-  berry.image.scale = 0.2;
+	berry = new Sprite(x, y, 20, 20);
+	berry.life = 60 * 5;
+	berry.vel.x = xVel;
+	berry.image = berryImage;
+	berry.image.scale = 0.2;
 
-  berry.update = () => {
-    berry.vel.x = xVel;
-  };
+	berry.update = () => {
+		berry.vel.x = xVel;
+		if (berry.collides(player)) {
+			score++;
+			berry.remove();
+		}
+	};
 
-  berryGroup.add(berry);
+	berryGroup.add(berry);
 }
