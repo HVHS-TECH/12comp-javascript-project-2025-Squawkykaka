77df3abad26ee51e4c9ffb06c06efbbf8e09b36e
Squawkykaka <squawkykaka@gmail.com>
2025-04-08 19:30:25 +1200

    Basically working game

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 [![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-2972f46106e565e64193e422d61a12cf1da4916b45550586e14ef0a7c637dd04.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=18486988)
+
+
+---
+Things to do:
+- [ ] make scoring system & save high score 
+- [x] make it so when you fall off screen you die
+- [x] fix berry collider
+- [ ] make background more interesting
+- [ ] change game to use 15 second timer and winning shows you the score over that 15 seconds
--- button.js
+++ button.js
@@ -1,5 +1,12 @@
+/**
+ * Spawns a button at specified coords with a specified function
+ * @param {String} text The text the button displays
+ * @param {int} x The x position of the button
+ * @param {int} y The y position of the button
+ * @param {Function} func The function ran when the button is pressed
+ */
 function spawnButton(text, x, y, func) {
-	let button = new Sprite(x, y, textWidth(text) + 100, 50, "k");
+	let button = new Sprite(x, y, textWidth(text) + 100, 50, 'k');
 	button.text = text;
 	button.textSize = 20;
 
